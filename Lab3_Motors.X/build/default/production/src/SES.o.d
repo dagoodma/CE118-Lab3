60060cad6ecfabab854c3f006aa94608752d787d
David Goodman <dagoodma@gmail.com>
2012-02-05 14:19:21 -0800

    part 2 completed, ready for part 3

---
+++ Lab3_Motors.X/build/default/production/src/SES.o.d
@@ -0,0 +1,2 @@
+build/default/production/src/SES.o: src/SES.c \
+ C:/Users/hahernan/MPLABXProjects/Lab3_Motors.X/include/ses.h
---
+++ Lab3_Motors.X/build/default/production/src/Stepper.o.d
@@ -0,0 +1,5 @@
+build/default/production/src/Stepper.o: src/Stepper.c \
+ C:/Users/hahernan/MPLABXProjects/Lab3_Motors.X/include/Stepper.h \
+ C:/Users/hahernan/MPLABXProjects/Lab3_Motors.X/include/PORTS.h \
+ C:/Users/hahernan/MPLABXProjects/Lab3_Motors.X/include/Serial.h \
+ C:/Users/hahernan/MPLABXProjects/Lab3_Motors.X/include/PORTS.h
