9bb57d9fade1877bf851e911caf50490fc627206
David Goodman <dagoodma@gmail.com>
2012-02-07 16:19:39 -0800

    Part 3 .7-.6 checked off

--- Lab3_Motors.X/main.c
+++ Lab3_Motors.X/main.c
@@ -13,14 +13,20 @@
 #include <pwm.h>
 #include <SES.H>
 #include <Stepper.h>
+//#include <string.h>
+//#include <stdlib.h>
+
 //#include <stdio.h>
 //include <LED.h>
 
-#define STEP_TIMER 1
-#define STEP_TIMER_DELAY 10
-#define STEPHI_TIMER_DELAY 10
+#define PORT_PWM PWM_PORTZ06
+#define FREQ_PWM 200
+#define USE_PWM
 
-#define uint unsigned int
+#define STEP_TIMER 1
+
+#define STEP_TIMER_DELAY 2
+#define STEPHI_TIMER_DELAY 2
 
 #define STEP_TRIS       PORTZ03_TRIS
 #define DIRECTION_TRIS  PORTZ04_TRIS
@@ -43,8 +49,8 @@ static enum {idle, cw, ccw, step} state = idle;
 
 
 // ---------------- Prototypes ------------------
-uint min(uint a, uint b);
-uint max(uint a, uint b);
+unsigned int min(unsigned int a, unsigned int b);
+unsigned int max(unsigned int a, unsigned int b);
 void StartIdle();
 void StartStep();
 void StartCw();
@@ -56,6 +62,11 @@ int main(void) {
     SERIAL_Init();
     TIMERS_Init();
     InitTimer(STEP_TIMER,1);
+
+#ifdef USE_PWM
+    PWM_Init(PORT_PWM, FREQ_PWM);
+    SetDutyCycle(PORT_PWM, 500);
+#endif
 
     // Initialize interrupts
     INTEnableSystemMultiVectoredInt();
@@ -69,7 +80,7 @@ int main(void) {
     StartIdle();
 
  
-    printf("\nHello, I am working...");
+    printf("\nHello, I am idling...");
 
     while (1) {
 
@@ -79,24 +90,32 @@ int main(void) {
                 // do nothing
                 break;
             case cw:
+                #ifndef USE_PWM
                 if (IsTimerExpired(STEP_TIMER)) {
                     StartStep();
+                // else do nothing
                 }
+                #endif
                 break;
             case ccw:
+                #ifndef USE_PWM
                 if (IsTimerExpired(STEP_TIMER)) {
                     StartStep();
+                    // else do nothing
                 }
+                #endif
                 break;
             case step:
+                #ifdef USE_PWM
+                printf("\nError: shouldn't be stepping!");
+                #endif
                 if (IsTimerExpired(STEP_TIMER)) {
-                    STEP = 0;
                     if (DIRECTION == FORWARD){
                         StartCw();
-                        printf("forward");
+                        //printf("forward");
                     } else{
                         StartCcw();
-                        printf("reverse");
+                        //printf("reverse");
                     }
                 }
         }
@@ -107,44 +126,88 @@ int main(void) {
         if (keyPressed != 0) {
             if (keyPressed == 'f') {
                 // forward
+                printf("\nforward");
                 StartCw();
             }
             else if(keyPressed == 'r') {
                  // reverse
+                printf("\nreverse");
                 StartCcw();
             }
             else if(keyPressed == 's') {
                 StartIdle();
             }
-        }
+            else if(keyPressed == 'q') {
+                printf("\nGoodbye!");
+                PWM_End();
+                return 0;
+            }
+#ifdef USE_PWM
+            else if(keyPressed == 't') {
+                unsigned short int stringMax = 7;
+                unsigned short int i = 0;
+                char charNumber[1];
+                charNumber[0] = 0;
+                char stringNumber[stringMax+1];
+                printf("\nPlease enter a frequency:");
+
+                while(charNumber[0] != 46) {
+                        charNumber[0] = GetChar();
+                        //printf("\nGot %s", charNumber);
+                        if ((charNumber[0] <= 57 && charNumber[0] >= 48)) {
+                            stringNumber[i] = charNumber[0];
+                            stringNumber[i+1] = '\0';
+                            //printf("\nAppended %c to '%s'",charNumber[0],stringNumber);
+                            //stringNumber = strcat(stringNumber, charNumber);
+                            i++;
+                        }
+
+                        if (i >= stringMax)
+                            break;
+                        
+                }
+                //printf("\ni=%u stmax=%u", i, stringMax);
+                printf("\nDone");
+                unsigned int newFreq = atoi(stringNumber);
+                printf("\nFrequency set to %u",newFreq);
+                PWM_End();
+                PWM_Init(PORT_PWM, newFreq);
+                SetDutyCycle(PORT_PWM, 500);
+            }
+#endif
+        } // end of keyPressed
  
-        while (!IsTransmitEmpty()); // bad, this is blocking code
-    }
+        //while (!IsTransmitEmpty()); // bad, this is blocking code
+    } // end of while loop
 
     return 0;
 }
 
 void StartIdle() {
-     printf("\nIdling...");
+     //printf("\nIdling...");
     state = idle;
     ENABLE = 0;
     STEP = 0;
 }
 
 void StartCw() {
-    printf("\nClockwising...");
-    ENABLE = 1;
+    //printf("\nClockwising...");
+    if (!ENABLE)
+        ENABLE = 1;
     STEP = 0;
-    DIRECTION = FORWARD;
+    if (DIRECTION != FORWARD)
+        DIRECTION = FORWARD;
     state = cw;
     InitTimer(STEP_TIMER,STEP_TIMER_DELAY);
 }
 
 void StartCcw() {
-    printf("\nCounter-Clockwising...");
-    ENABLE = 1;
+    //printf("\nCounter-Clockwising...");
+    if (!ENABLE)
+        ENABLE = 1;
     STEP = 0;
-    DIRECTION = REVERSE;
+    if (DIRECTION != REVERSE)
+        DIRECTION = REVERSE;
     state = ccw;
     InitTimer(STEP_TIMER,STEP_TIMER_DELAY);
 }
@@ -166,7 +229,7 @@ void StartStep() {
  * @return Lowest value, either a or b
  * @remark
  */
-uint min(uint a, uint b) {
+unsigned int min(unsigned int a, unsigned int b) {
     if (a < b)
         return a;
     else
@@ -180,7 +243,7 @@ uint min(uint a, uint b) {
  * @return Highest value, either a or b
  * @remark
  */
-uint max(uint a, uint b) {
+unsigned int max(unsigned int a, unsigned int b) {
     if (a > b)
         return a;
     else
